ac6a3796ba689ae15fcbe420447a0cdc41f9bc13
Chris Smith <116120269+k2aviator@users.noreply.github.com>
2023-05-29 21:23:22 -0700

    finished week 7 assignment

--- models/weather.js
+++ models/weather.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const weatherSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true  },
+  name: { type: String, required: true, unique: true },
   temperature: { type: Number, required: true  }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+router.use('/landing', require('./landing'))
+router.use('/weather', require('./weather'))
+router.use('/weather/location', require('./weatherLocation'))
 
 module.exports = router;
